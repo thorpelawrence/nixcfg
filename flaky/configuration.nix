b3f8d4ceaeb3db75ae34b6084c6131de9df9908f
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2024-01-08 22:27:10 +0000

    remove fin exit node

--- flaky/configuration.nix
+++ flaky/configuration.nix
@@ -95,7 +95,6 @@
     in {
       containers = {}
         // tailscale_exit_node_mullvad { countries = [ "UK" ]; cities = [ "London" ]; type = "wireguard"; shortname = "lon"; }
-        // tailscale_exit_node_mullvad { countries = [ "Finland" ]; shortname = "fin"; }
         // tailscale_exit_node_mullvad { countries = [ "Norway" ]; type = "wireguard"; shortname = "nor"; };
     };
     podman = {
