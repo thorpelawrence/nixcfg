ee0acc1a77747e93308facb2a076c632949d0a9b
Lawrence Thorpe <lawrence@lawrencethorpe.uk>
2024-07-02 20:47:28 +0100

    plik, pcopy proxy

--- hosts/flaky/hosting.nix
+++ hosts/flaky/hosting.nix
@@ -1,6 +1,17 @@
 { pkgs, lib, config, ... }:
+let
+  domain = "letc.me";
+in
 {
   services.caddy = {
     enable = true;
+    virtualHosts  = {
+      "plik.${domain}".extraConfig = ''
+        reverse_proxy http://100.88.233.89:1612
+      '';
+      "pcopy.${domain}".extraConfig = ''
+        reverse_proxy http://100.88.233.89:2586
+      '';
+    };
   };
 }
