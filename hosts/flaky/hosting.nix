337b218e9286ccc671747b8b39a181f1c3a8fa5f
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2026-06-19 12:57:23 +0100

    go vanity domain

--- hosts/flaky/hosting.nix
+++ hosts/flaky/hosting.nix
@@ -11,6 +11,34 @@ in
   services.caddy = {
     enable = true;
     virtualHosts = {
+      "go.${domain}".extraConfig = ''
+        vars upstream "https://github.com/thorpelawrence"
+
+        handle / {
+          redir {vars.upstream}
+        }
+
+        handle /* {
+          @goget query go-get=1
+
+          vars repo_name {path.0}
+
+          handle @goget {
+            header Content-Type "text/html; charset=utf-8"
+            respond <<HTML
+              <html>
+                <head>
+                  <meta name="go-import" content="https://{host}/{vars.repo_name} git {vars.upstream}/{vars.repo_name}">
+                </head>
+              </html>
+              HTML
+          }
+
+          handle {
+            redir https://pkg.go.dev/{host}{uri}
+          }
+        }
+      '';
       "ok.${domain}".extraConfig = ''
         reverse_proxy http://100.71.198.32:11212
       '';
